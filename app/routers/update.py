3920ed3f794a09e704db3e590964855a631d1052
Rodrigo Minguell <rodrigo.minguell@yahoo.com>
2025-04-22 00:57:59 +0200

    debug 3

--- app/routers/update.py
+++ app/routers/update.py
@@ -1,11 +1,11 @@
 from fastapi import APIRouter, Depends
 from fastapi.responses import JSONResponse
-from app.inference import load_model
+from app.inference import get_model
 from app.dependencies import authenticate
 
 router = APIRouter()
 
 @router.get("/update")
 def update_model(user: str = Depends(authenticate)):
-    load_model(update=True)
+    get_model(update=True)
     return JSONResponse(content={"message": "Model updated successfully"})
